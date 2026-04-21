5559e4754c0a474e49fcfa7fd4c028f81d2af9d2
YangZhen <yangnade23@gmail.com>
2026-04-21 23:19:21 +0800

    添加 2235-两数相加

---
+++ 2235-两整数相加.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
+
+if __name__ == '__main__':
+    s = Solution()
+    ans = s.sum(1,2)
+    assert ans == 3, "错了"
